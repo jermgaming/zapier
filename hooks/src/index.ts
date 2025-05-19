9d9f9fa5208d232ae515539f88d4b31678d66c11
jermgaming <atinder999@gmail.com>
2025-05-19 14:55:00 +0530

    hooks_completed

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,14 +1,40 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client= new PrismaClient();
 
 const app=express();
+app.use(express.json())
 
-app.post("/hooks/catch/:userId/:zapId",(req,res)=>{
+app.post("/hooks/catch/:userId/:zapId",async (req,res)=>{
     const userId = req.params.userId;
     const zapId = req.params.zapId;
+    const body = req.body;
 
     //store a db in new trigger
+    await client.$transaction(async tx=>{
+        const run = await client.zapRun.create({
+            data:{
+                zapId : zapId,
+                metadata : body
+            }
+        });
 
-    //push it on to a queue (Kafka/redis)
+        await client.zapRunOutbox.create({
+            data:{
+                zapRunId : run.id
+            }
+        })
+        res.json({
+            message:"Webhook received"
+        })
+    })
     
 
+    //push it on to a queue (Kafka/redis)
+
+
 })
+
+
+app.listen(3000);
